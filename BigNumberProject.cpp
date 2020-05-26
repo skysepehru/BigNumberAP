c13916fdfeda8f9ce816a51c56a06cc27250c17c
skysepehr <skysepehru@gmail.com>
2020-05-26 12:03:28 +0430

    main updated

--- BigNumberProject.cpp
+++ BigNumberProject.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-	MyBigNumber big1 = (long)453;
-	cout<< (big1<<23);
+	BigNumber big1 = "153";
+	BigNumber big2 = "124";
+	BigNumber big3 = "1554612";
+	BigNumber big4 = "-115456";
+	cout << "Testing Assignment:" << endl;
+	big2 = big1;
+	cout << big2 << endl;
+	cout << "Testing Subtraction:" << endl;
+	cout << big3 - big4 << endl;
+	cout << "Testing ++ and -- " << endl;
+	cout << big3-- << endl;
+	cout << big3 << endl;
+	cout << ++big4 << endl;
+	MyBigNumber big5 = "999";
+	cout << "Testing multByOneDigit" << endl;
+	cout << big5.multByOneDigit(9) << endl;
+	cout << " Testing <<" << endl;
+	cout << (big5 << 6) << endl;
 }
